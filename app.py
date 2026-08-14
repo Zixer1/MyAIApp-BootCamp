eae5d4acd7644559b611bc2b84de764fedf0ea4d
Alexandre Sakr <alexandersakr@gmail.com>
2026-08-14 12:23:22 -0400

    Working ai within web

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
 
 st.title("My AI app")
-
 
 with st.sidebar:
     st.header("Settings")
@@ -11,3 +14,18 @@ with st.sidebar:
     if st.button("Save"):
         st.write(f"Saved, your name is {name}, your mood is {mood}, and your creativity is {creativity}")
 
+prompt = st.chat_input("Ask me something.. ")
+
+if prompt:
+    client = OpenAI(
+        base_url="https://api.groq.com/openai/v1",
+        api_key=os.getenv("GITHUB_TOKEN"),
+    )
+    r = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}],
+    )
+    with st.chat_message("user"):
+        st.write(prompt)
+    with st.chat_message("assistant"):
+        st.write(f"{r.choices[0].message.content}")
