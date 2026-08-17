f9e6997d50a73b5abdbb0fed1faf70b210c5a9e8
Alexandre Sakr <alexandersakr@gmail.com>
2026-08-17 11:47:10 -0400

    Changed files

--- app.py
+++ app.py
@@ -9,23 +9,25 @@ st.title("My AI app")
 with st.sidebar:
     st.header("Settings")
     name = st.text_input("Enter your name")
-    mood = st.selectbox("What will your AI's mood be?", ["Happy", "Sad", "Angry"])
     creativity = st.slider("Creativity", 0.0, 1.0, 0.3)
+    model = st.selectbox("Model", ["openai/gpt-oss-120b", "openai/gpt-oss-20b"])
     if st.button("Save"):
-        st.write(f"Saved, your name is {name}, your mood is {mood}, and your creativity is {creativity}")
+        st.write(f"Saved, your name is {name}, and your creativity is {creativity}")
 
-prompt = st.chat_input("Ask me something.. ")
+prompt = st.chat_input("Ask something in here.. ")
 
 if prompt:
     client = OpenAI(
-        base_url="https://api.groq.com/openai/v1",
+        base_url="https://api.groq.com/openai/v1",  # <-Check ZOOM chat for change
         api_key=os.getenv("GITHUB_TOKEN"),
-    )
-    r = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[{"role": "user", "content": prompt}],
     )
     with st.chat_message("user"):
         st.write(prompt)
     with st.chat_message("assistant"):
-        st.write(f"{r.choices[0].message.content}")
+        with st.spinner("Thinking..."):
+             r = client.chat.completions.create(
+                model=model,
+                temperature=creativity,
+                messages=[{"role": "user", "content": prompt}]
+             )
+        st.write(f" {r}")
